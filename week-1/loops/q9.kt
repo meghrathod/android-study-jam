b4b9ec34f6fe2fe952cc85be8cc635952fc1cd32
meghrathod <msr.megh@gmail.com>
2020-12-15 16:30:19 +0530

    Update q9.kt

--- week-1/loops/q9.kt
+++ week-1/loops/q9.kt
@@ -1,9 +1,11 @@
 fun main(){
-    var num = 4
-    for (i in 1..num){
-        for (x in 1..i){
-            print("#")
-        }
-        println()
+    var num = 17
+    var sum = 0
+    if (num%2 == 1){
+        num -= 1
     }
+    for (i in 0..num step 2){
+        sum += i
+    }
+    println("Your sum is ${sum}")
 }
