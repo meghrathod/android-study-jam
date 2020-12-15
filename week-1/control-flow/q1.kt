3718063dd63a339cef0873f70779695b1df97f32
meghrathod <msr.megh@gmail.com>
2020-12-15 14:41:23 +0530

    Create q1.kt

---
+++ week-1/control-flow/q1.kt
@@ -0,0 +1,17 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+    val num = 0
+    if (num>0){
+        println("Number is positive.")
+    }
+    else if (num<0){
+        println("Number is negative.")
+    }
+    else{
+        println("Number is 0.")
+    }
+}
