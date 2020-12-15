8cb2c46ef3cde816460181ae29eb63db340b9b37
meghrathod <msr.megh@gmail.com>
2020-12-15 15:01:09 +0530

    Create q4.kt

---
+++ week-1/control-flow/q4.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var num = 110
+    if (num%11 == 0 && num%5 == 0){
+        println("Number is divisible by 5 and 11")
+    }
+    else{
+        println("Number is not divisible by 5 and 11")
+    }
+    
+}
